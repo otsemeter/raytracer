41947143ebb575d291fe1d8ca30a57c63ead8ddc
Owen Smeter <owensemeter@gmail.com>
2026-02-12 10:27:30 -0800

    added camera parameters, viewport, and refactored img generation in terms of ray color with gradient along y axis

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
 #include <iostream>
-void generate_ppm_rg(int, int, int);
+void generate_ppm_rg();
+color ray_color(const ray& r);
 
 /*
 PPM image file format:
@@ -21,11 +23,34 @@ r<width * height> g<width * height> b<width * height>
 EOF
 
 */
-void generate_ppm_rg(int x, int y, int z) {
+void generate_ppm_rg() {
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    auto aspect_ration = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate image height, ensure atleast 1
+    int image_height = int(image_width / aspect_ration);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // calc the vectors across the horizontal and down the vertical viewport edges
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // calc the horizontal and vertical delta vectors from pixel to pixel
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // calc the location of the upper left pixel
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -34,10 +59,11 @@ void generate_ppm_rg(int x, int y, int z) {
     for (int j = 0; j < image_height; j++) {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto pixel_color = color(double(i) / (image_width-1) * x,
-            double(j) / (image_height-1) * y,
-            z * (double(i) / (image_width-1) * x + double(j) / (image_height-1) * y));
-
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+            
+            color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
     }
@@ -45,7 +71,13 @@ void generate_ppm_rg(int x, int y, int z) {
     std::clog << "\rDone.                       \n";
 }
 
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
+
 
 int main() {
-    generate_ppm_rg(1,0,0);
+    generate_ppm_rg();
 }
